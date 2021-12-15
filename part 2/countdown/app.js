4db7a481fa09f6c796c5a7b868fb9c49087522be
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2021-12-15 07:40:29 -0800

    complete this task

--- part 2/countdown/app.js
+++ part 2/countdown/app.js
@@ -28,7 +28,7 @@ const  count =document.querySelector(".countbox");
 const countBox = document.querySelectorAll(".countbox1 h4")
 
 let futureData = new Date(2021,11,15,6,38,0);
-console.log(futureData); 
+// console.log(futureData); 
 
 const year = futureData.getFullYear();
 const hours = futureData.getHours();
@@ -50,18 +50,36 @@ const futureTime = futureData.getTime();
 
 function getReminingTime(){
   const today = new Date().getTime();
-  const t = today - futureTime ; 
-  console.log(t)
+  const t =   today - futureData ; 
+  // console.log(t)
 
   const oneDay = 24*60*60*1000;
   const onehours = 60*60*1000;
   const oneMin = 60*1000; 
   let day = t/oneDay;
   day = Math.floor(day);
-let hour = (t % oneDay)/onehours;
-console.log(hour);
+  let hours = Math.floor((t % oneDay) / onehours);
+  let minutes = Math.floor((t % onehours) / oneMin);
+  let seconds = Math.floor((t % oneMin) / 1000);
+  const values = [day, hours, minutes, seconds];
+  function format(item) {
+    if (item < 10) {
+      return (item = `0${item}`);
+    }
+    return item;
+  }
 
+  countBox.forEach(function (item, index) {
+    item.innerHTML = format(values[index]);
+  });
+
+  if (t < 0) {
+    clearInterval(countdown);
+    countBox.innerHTML = `<h4 class="expired">sorry, this giveaway has expired!</h4>`;
+  }
 
 }
+let countdown = setInterval(getReminingTime, 1000);
+//set initial values
 
 getReminingTime();
